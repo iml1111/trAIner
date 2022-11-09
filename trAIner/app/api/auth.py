de1b2031bde095f5b77c3ab8a26d95488d206e6d
Taebo Won <asun291@naver.com>
2022-11-09 23:25:34 +0900

    temporary commit

--- trAIner/app/api/auth.py
+++ trAIner/app/api/auth.py
@@ -4,10 +4,6 @@ from app.api.response import response_200, bad_request, created
 from app.api.decorator import timer
 from model.mongodb import User, MasterConfig, Log
 from flask import Blueprint
-from werkzeug.security import (
-    generate_password_hash,
-    check_password_hash
-)
 from flask_jwt_extended import (
     get_jwt_identity,
     create_refresh_token,
@@ -30,7 +26,7 @@ def sign_in(
     user = User(current_app.db).get_password_with_id(id)
     if not user or user['password'] is None:
         return bad_request("Authentication failed.")
-    if not check_password_hash(user['password'], pw):
+    if user['password'] != pw:
         return bad_request("Authentication failed.")
 
     user_oid = str(user['_id'])
