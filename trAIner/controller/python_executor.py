1dcbbb5b04290591f4db69a8152d7f52519711b3
IML <shin10256@gmail.com>
2022-11-21 00:52:27 +0900

    update: python executor
    input_data 및 timeout args 추가

--- trAIner/controller/python_executor.py
+++ trAIner/controller/python_executor.py
@@ -1,39 +1,28 @@
 import subprocess
+from subprocess import TimeoutExpired
 
 
-def execute(script: str) -> str:
-	output = subprocess.check_output(["python", "-c", script]).decode("utf-8").strip()
-	return output
+def execute(script: str, input_str: str, timeout: int = 10) -> str:
+	"""Execute the given script with the given input and return the output."""
+	output = subprocess.run(
+		["python3", "-c", script],
+		input=input_str,
+		capture_output=True,
+		encoding="utf-8",
+		timeout=timeout,
+	)
+	return output.stdout
 
 
 if __name__ == '__main__':
 	output = execute("""
-def solution(n, arr1, arr2):
-    pattern = [' ', '#']
-    result = []
-    for i in range(n) :
-        decoded = ''
-        union = arr1[i] | arr2[i]
-        for _ in range(n):
-            decoded = pattern[union % 2] + decoded
-            union //= 2
-        result.append(decoded)
-    
-    return result
+input_str = input()
+input_str2 = input()
 
-
-if __name__ == '__main__':
-    result = solution(
-        6,
-        [46, 33, 33 ,22, 31, 50],
-        [27 ,56, 19, 14, 14, 10]
-    )
-    print(result)
-    print(result)
-""")
-	# 결과는 무조건 str으로 출력됨.
-	print(output)
-	print(type(output))
-	# 복수의 개행 문자의 경우, \n으로 오며, 아래와 같이 split()로 다룰 수도 있음.
-	print(output.split("\n"))
+int1, int2 = input_str.split()
+int1, int2 = int(int1), int(int2)
+print(int1 + int2)
+print(input_str2.split())
+""", "1 2\nasd asd")
+	print(output, type(output))
 	
